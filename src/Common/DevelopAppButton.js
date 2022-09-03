b28ffde0e14158dac8d24485da9b6d43dfac9592
ingenmaffen <ingenmaffen@gmail.com>
2022-09-03 11:48:55 +0200

    Add new React buttons to developPanel

---
+++ src/Common/DevelopAppButton.js
@@ -0,0 +1,5 @@
+import './DevelopAppButton.css';
+
+export const DevelopAppButton = ({text}) => (
+    <button className="develop-app-button">{text}</button>
+);
--- src/Layout/DevelopPanel.js
+++ src/Layout/DevelopPanel.js
@@ -1,3 +1,10 @@
+import './DevelopPanel.css';
+import { DevelopAppButton } from "../Common/DevelopAppButton";
+
 export const DevelopPanel = () => (
-    <button>Develop an App</button>
+    <div className="develop-panel">
+        <DevelopAppButton text="Develop a smaller app" time={60} valueMultiplier={1} developersRequired={0} />
+        <DevelopAppButton text="Develop a medium app" time={150} valueMultiplier={2.5} developersRequired={3} />
+        <DevelopAppButton text="Develop a large app" time={300} valueMultiplier={10} developersRequired={5} />
+    </div>
 );

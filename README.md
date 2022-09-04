0a1e378fb22c0878efccde8ca3c62694a8160e4f
ingenmaffen <ingenmaffen@gmail.com>
2022-09-04 19:47:51 +0200

    Add styling for header

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ For the sake of retain simplicity, I will not add Redux to the code base until i
 ## Acknowledgement
 
 - Favicon: [Freepik @ Flaticon (coding)](https://www.flaticon.com/free-icon/coding_1159283)
+- Header font: [Computer pixel](https://www.1001freefonts.com/computer-pixel-7.font)
 - Resources Panel Icons: [Freepik @ Flaticon (web development icon pack)](https://www.flaticon.com/packs/web-development-19)
     - [Developer](https://www.flaticon.com/free-icon/coding_921579)
     - [Tester](https://www.flaticon.com/free-icon/bug_921564)
--- src/Layout/AppHeader.jsx
+++ src/Layout/AppHeader.jsx
@@ -2,10 +2,13 @@ import "./AppHeader.css";
 
 export const AppHeader = ({money}) => (
     <>
-        <h1 class="title">Dev Clicker</h1>
-        <div class="info-container">
-            <div class="placeholder"></div>
-            <div class="money-container">${money}</div>
+        <h1 className="title">
+            <img className="logo" src={"logo512.png"} alt={"logo"} />
+            <span>Dev Clicker</span>
+        </h1>
+        <div className="info-container">
+            <div className="placeholder"></div>
+            <div className="money-container">$ {money}</div>
         </div>
     </>
 )

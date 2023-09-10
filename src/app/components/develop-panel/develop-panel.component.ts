67d0fb76354b9ce0dbd598c0fee2a9853897478f
Rabi Róbert <ingenmaffen@gmail.com>
2023-09-10 14:31:46 +0200

    Copy over develop panel html and css

--- src/app/components/develop-panel/develop-panel.component.ts
+++ src/app/components/develop-panel/develop-panel.component.ts
@@ -5,4 +5,40 @@ import { Component } from "@angular/core";
     templateUrl: "./develop-panel.component.html",
     styleUrls: ["./develop-panel.component.scss"],
 })
-export class DevelopPanelComponent {}
+export class DevelopPanelComponent {
+    public appTypes = [
+        {
+            type: "small",
+            icon: "assets/block.png",
+            time: 60,
+            valueMultiplier: 1,
+            requiredDevelopers: 0,
+        },
+        {
+            type: "medium",
+            icon: "assets/blockchain.png",
+            time: 150,
+            valueMultiplier: 2.5,
+            requiredDevelopers: 3,
+        },
+        {
+            type: "large",
+            icon: "assets/blocks.png",
+            time: 300,
+            valueMultiplier: 10,
+            requiredDevelopers: 5,
+        },
+    ];
+
+    public developApp(appType: string): void {
+        console.log(appType);
+    }
+
+    public isButtonDisabled(appType: string): boolean {
+        return false;
+    }
+
+    public getDevelopmentTime(appType: string): string | null {
+        return null;
+    }
+}

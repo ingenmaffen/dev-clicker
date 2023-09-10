c45dcc5679e2236b6e442756c0636c44e5d1ad68
Rabi Róbert <ingenmaffen@gmail.com>
2023-09-10 20:36:08 +0200

    Copy over most of sell-panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { DevelopPanelComponent } from './components/develop-panel/develop-panel.component';
-import { SellPanelComponent } from './components/sell-panel/sell-panel.component';
-import { ResourcePanelComponent } from './components/resource-panel/resource-panel.component';
-import { PriceChartComponent } from './components/sell-panel/price-chart/price-chart.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { DevelopPanelComponent } from "./components/develop-panel/develop-panel.component";
+import { SellPanelComponent } from "./components/sell-panel/sell-panel.component";
+import { ResourcePanelComponent } from "./components/resource-panel/resource-panel.component";
+import { PriceChartComponent } from "./components/sell-panel/price-chart/price-chart.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DevelopPanelComponent,
-    SellPanelComponent,
-    ResourcePanelComponent,
-    PriceChartComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HeaderComponent, DevelopPanelComponent, SellPanelComponent, ResourcePanelComponent, PriceChartComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/sell-panel/sell-panel.component.ts
+++ src/app/components/sell-panel/sell-panel.component.ts
@@ -5,4 +5,36 @@ import { Component } from "@angular/core";
     templateUrl: "./sell-panel.component.html",
     styleUrls: ["./sell-panel.component.scss"],
 })
-export class SellPanelComponent {}
+export class SellPanelComponent {
+    public appTypes = [
+        {
+            type: "small",
+            icon: "assets/block.png",
+            time: 60,
+            valueMultiplier: 1,
+            requiredDevelopers: 0,
+        },
+        {
+            type: "medium",
+            icon: "assets/blockchain.png",
+            time: 150,
+            valueMultiplier: 2.5,
+            requiredDevelopers: 3,
+        },
+        {
+            type: "large",
+            icon: "assets/blocks.png",
+            time: 300,
+            valueMultiplier: 10,
+            requiredDevelopers: 5,
+        },
+    ];
+
+    public selectedAppType: string = "small";
+
+    public get isSellButtonDisabled(): boolean {
+        return false;
+    }
+
+    public sellApp(): void {}
+}

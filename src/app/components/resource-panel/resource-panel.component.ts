1f84f4aff5bba83d710db79a1a2c6293297d8a8e
Rabi Róbert <ingenmaffen@gmail.com>
2023-09-12 21:02:38 +0200

    Add resource panel and move common button style

--- src/app/components/resource-panel/resource-panel.component.ts
+++ src/app/components/resource-panel/resource-panel.component.ts
@@ -5,4 +5,29 @@ import { Component } from "@angular/core";
     templateUrl: "./resource-panel.component.html",
     styleUrls: ["./resource-panel.component.scss"],
 })
-export class ResourcePanelComponent {}
+export class ResourcePanelComponent {
+    public humanResources = [
+        {
+            type: "developer",
+            displayText: "Developer",
+            icon: "assets/coding.png",
+        },
+        {
+            type: "tester",
+            displayText: "Tester",
+            icon: "assets/bug.png",
+        },
+        {
+            type: "businessAnalyst",
+            displayText: "Business Analyst",
+            icon: "assets/analytics.png",
+        },
+        {
+            type: "productOwner",
+            displayText: "Product Owner",
+            icon: "assets/user.png",
+        },
+    ];
+
+    public hire(resourceType: string): void {}
+}
